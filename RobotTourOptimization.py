166534aa43a4db492179ecf3d48d62b582771dcb
galiixy <gaelle_f@live.fr>
2019-12-23 17:37:37 +0100

    Correction qualite du code

--- RobotTourOptimization.py
+++ RobotTourOptimization.py
@@ -15,28 +15,28 @@ def calcul_distance(first_point_value, second_point_value):
 def calcul_circuit(list_of_points, cycle):
     distance = 0
     i = 0
-    for i in range (len(cycle)):
-        if (i == (len(cycle) - 1)):
-            distance +=calcul_distance(list_of_points[cycle[i]], list_of_points[cycle[0]])
+    for i in range(len(cycle)):
+        if(i == (len(cycle)-1)):
+            distance += calcul_distance(list_of_points[cycle[i]], list_of_points[cycle[0]])
         else:
-            distance +=calcul_distance(list_of_points[cycle[i]], list_of_points[cycle[i + 1]])
+            distance += calcul_distance(list_of_points[cycle[i]], list_of_points[cycle[i+1]])
     return distance
 
 
-def Fusion(tab1,tab2):
-    n1=len(tab1) 
-    n2= len(tab2)
-    n= n1+n2
-    j=1
-    k=1
-    tab=[0]*(n)
+def Fusion(tab1, tab2):
+    n1 = len(tab1)
+    n2 = len(tab2)
+    n = n1+n2
+    j = 1
+    k = 1
+    tab = [0]*(n)
     for i in range(n):
-        if((j<=n1) and ((k>n2) or (tab1[j-1][1]<=tab2[k-1][1]))):
+        if((j <= n1) and ((k > n2) or (tab1[j-1][1] <= tab2[k-1][1]))):
             tab[i] = tab1[j-1]
-            j=j+1
+            j = j+1
         else:
-            tab[i]=tab2[k-1]
-            k=k+1
+            tab[i] = tab2[k-1]
+            k = k+1
     return tab
 
 
@@ -44,22 +44,17 @@ def MergeSort(tab):
     longueurtab = len(tab)
     n1 = round(longueurtab/2)
     n2 = longueurtab-n1
-    
-    if (longueurtab==1):
+    if(longueurtab == 1):
         return tab
     else:
-        tab1=[0]*(n1)
-        tab2=[0]*(n2)
-        
+        tab1 = [0]*(n1)
+        tab2 = [0]*(n2)
         for i in range(n1):
-            tab1[i] =tab[i]
-            
+            tab1[i] = tab[i]
         for j in range(n2):
-            tab2[j]=tab[n1+j]
-            
+            tab2[j] = tab[n1+j]
         tab1 = MergeSort(tab1)
         tab2 = MergeSort(tab2)
-        
         tab = Fusion(tab1, tab2)
     return tab
 
@@ -71,12 +66,12 @@ def nearest_neighbor_algorithm(first_point, list_of_points):
     visited = list()
     visited.append(first_point)
     p = p0
-    while len(unvisited) != 0 :
+    while len(unvisited) != 0:
         res = [0]*(len(unvisited))
         i = 0
         for point in unvisited:
-            res[i] = (point,  calcul_distance(p, unvisited[point]),unvisited[point])
-            i=i+1
+            res[i] = (point, calcul_distance(p, unvisited[point]), unvisited[point])
+            i = i+1
         res = MergeSort(res)
         p = res[0]
         visited.append(p[0])
@@ -85,88 +80,67 @@ def nearest_neighbor_algorithm(first_point, list_of_points):
 
 
 def get_MatriceDistance(first_point, list_of_points, matrice_distance, longueur):
-    matrice_point=[0]*(longueur)
-    
-    pointReference=list_of_points[first_point]
+    matrice_point = [0]*(longueur)
+    pointReference = list_of_points[first_point]
     points = copy.copy(list_of_points)
-   
     del points[first_point]
     matrice_distance[0][0] = first_point
-
     for cpt_ligne in range(longueur):
-        matrice_point[0]= list_of_points[cpt_ligne]
+        matrice_point[0] = list_of_points[cpt_ligne]
         matrice_distance[cpt_ligne][0] = cpt_ligne
         cpt_colonne = 1
         for index_point in points:
             matrice_distance[cpt_ligne][cpt_colonne] = (calcul_distance(pointReference, points[index_point]))
-            matrice_point[cpt_colonne]= points[index_point]
+            matrice_point[cpt_colonne] = points[index_point]
             cpt_colonne = cpt_colonne + 1
-        points[longueur+cpt_ligne]=pointReference
-        pointReference=matrice_point[1]
-        del points[cpt_ligne+1
+        points[longueur+cpt_ligne] = pointReference
+        pointReference = matrice_point[1]
+        del points[cpt_ligne+1]
     return matrice_distance
 
 
 def great_algorithm(first_point, list_of_points):
-    longueur=len(list_of_points)
-    matrice_distance = np.zeros((longueur,longueur))
-    
+    longueur = len(list_of_points)
+    matrice_distance = np.zeros((longueur, longueur))
     distance_min = 100000
-    distance_max=0
-    
+    distance_max = 0
     points = list()
-    
     matrice_distance = get_MatriceDistance(first_point, list_of_points, matrice_distance, longueur)
-    
-    for cpt_colonne in range(1,longueur):
-        if(matrice_distance[first_point][cpt_colonne]>distance_max):
-            cpt_point = first_point+cpt_colonne
+    for cpt_colonne in range(1, longueur):
+        if(matrice_distance[first_point][cpt_colonne] > distance_max):
+            cpt_point = first_point + cpt_colonne
             if(cpt_point >= longueur):
-                    cpt_point = cpt_point - longueur
-            distance_max= matrice_distance[first_point][cpt_colonne]
-    
+                    cpt_point = cpt_point-longueur
+            distance_max = matrice_distance[first_point][cpt_colonne]
     points.append(first_point)
     points.append(cpt_point)
-                   
-    while(len(points)!=longueur):
+    while(len(points) != longueur):
         distance_min = 100000
-        index_min=0
-        
-        for cpt_colonne in range(1,longueur):
-            
-            index_min = cpt_point+cpt_colonne
-            
+        index_min = 0
+        for cpt_colonne in range(1, longueur):
+            index_min = cpt_point + cpt_colonne
             if(index_min >= longueur):
                     index_min = index_min - longueur
-
-            if(len(points)==(longueur-1) and index_min not in points):
-                index_point=index_min
-             
-            if(matrice_distance[cpt_point][cpt_colonne]<=distance_min and index_min not in points):
+            if(len(points) == (longueur-1) and index_min not in points):
+                index_point = index_min
+            if(matrice_distance[cpt_point][cpt_colonne] <= distance_min and index_min not in points):
                 distance_min = matrice_distance[cpt_point][cpt_colonne]
-                index_point=index_min
-                
+                index_point = index_min
         points.append(index_point)
         cpt_point = index_point
-    
     return list(points)
 
 
 def optimal_algorithm(first_point, list_of_points):
     distance_circuit = 10000
-    circuit= copy.copy(list_of_points)
-
+    circuit = copy.copy(list_of_points)
     del circuit[0]
-
-    permutations=list(itertools.permutations(circuit)) 
-
+    permutations = list(itertools.permutations(circuit))
     for circuit_permute in permutations:
-        circuit_permute=(first_point,) + circuit_permute 
-        
-        if(calcul_circuit(list_of_points,circuit_permute)<distance_circuit):
-            distance_circuit =calcul_circuit(list_of_points,circuit_permute)
-            circuitOptimal=circuit_permute
-
+        circuit_permute = (first_point,) + circuit_permute
+        if(calcul_circuit(list_of_points, circuit_permute) < distance_circuit):
+            distance_circuit = calcul_circuit(list_of_points, circuit_permute)
+            circuitOptimal = circuit_permute
     return list(circuitOptimal)
 
 
